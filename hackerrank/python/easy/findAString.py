a87e21891f99373d23c7df056331d63b43642ce7
schelqi <contact.sabahchelqi@gmail.com>
2025-05-27 17:57:51 +0100

    Python: Find a string

--- hackerrank/python/easy/findAString.py
+++ hackerrank/python/easy/findAString.py
@@ -1,14 +1,18 @@
 def count_substring(string, sub_string):
     start = 0
     count = 0
-    while string.find(sub_string, start) != -1:
+
+    while True:
+        start = string.find(sub_string, start)
+        if start == -1:
+            break
+        start += 1
         count += 1
-        start = string.find(sub_string, start) + 1
 
     return count
 
 
 if __name__ == '__main__':
-    string = input().strip()
-    sub_string = input().strip()
+    string = "ABCDCDC"
+    sub_string = "CDC"
     print(count_substring(string, sub_string))
