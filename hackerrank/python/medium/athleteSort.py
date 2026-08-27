0f9ee9f2037ef63f49c7813556e5b96de5df2347
schelqi <contact.sabahchelqi@gmail.com>
2026-08-27 16:29:12 +0100

    Athlete Sort

--- hackerrank/python/medium/athleteSort.py
+++ hackerrank/python/medium/athleteSort.py
@@ -1,11 +1,10 @@
 
 if __name__ == '__main__':
     n, m = map(int, input().split())
-    arr = []
+    data = list()
     for _ in range(n):
-        arr.append(list(map(int, input().rstrip().split())))
-
+        data.append(list(map(int, input().split())))
     k = int(input())
-    sorted_arr = sorted(arr, key=lambda x: x[k])
-    for item in sorted_arr:
-        print(*item)
+    sorted_data = sorted(data, key=lambda x: x[k])
+    for row in sorted_data:
+        print(*row)
