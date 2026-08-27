0edff2577baa3408d0b8a88e83371ba6dfaa488f
schelqi <contact.sabahchelqi@gmail.com>
2026-08-27 15:49:48 +0100

    Regex Substitution

--- hackerrank/python/medium/regexSubstitution.py
+++ hackerrank/python/medium/regexSubstitution.py
@@ -1,9 +1,14 @@
 import re
 
-for _ in range(int(input())):
-    # change && → and
-    modified_input_1 = re.sub(r"(?<=(\s))&&\s", "and ", input())
-    # change || → or
-    modified_input_2 = re.sub(r"(?<=(\s))\|\|\s", "or ", modified_input_1)
-    # print result
-    print(modified_input_2)
+if __name__ == '__main__':
+
+    pattern_and = re.compile(r"(?<=(\s))&&\s")
+    pattern_or = re.compile(r"(?<=(\s))\|\|\s")
+
+    for _ in range(int(input())):
+        # change && → and
+        modified_input_1 = re.sub(pattern_and, "and ", input())
+        # change || → or
+        modified_input_2 = re.sub(pattern_or, "or ", modified_input_1)
+        # print result
+        print(modified_input_2)
