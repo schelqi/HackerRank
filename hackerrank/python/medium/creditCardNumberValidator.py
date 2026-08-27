c22b26088ca12e3313562237c224229a7ba17312
schelqi <contact.sabahchelqi@gmail.com>
2026-08-27 15:39:29 +0100

    Validating Credit Card Numbers

--- hackerrank/python/medium/creditCardNumberValidator.py
+++ hackerrank/python/medium/creditCardNumberValidator.py
@@ -1,12 +1,15 @@
 import re
 
-for _ in range(int(input())):
-    credit_card_number = input().strip()
-    if re.match(r"^[4-6]\d{3}-?\d{4}-?\d{4}-?\d{4}-?$", credit_card_number):
-        credit_card_number_modified = re.sub(r"-", "", credit_card_number)
-        if re.search(r"(\d)\1{3}", credit_card_number_modified):
-            print("Invalid")
+if __name__ == '__main__':
+    credit_card_pattern = re.compile(r"^[4-6]\d{3}-?\d{4}-?\d{4}-?\d{4}$")
+    consecutive_digits_pattern = re.compile(r"(\d)\1{3}")
+    for _ in range(int(input())):
+        credit_card_number = input().strip()
+        if re.match(credit_card_pattern, credit_card_number):
+            credit_card_number_modified = re.sub(r"-", "", credit_card_number)
+            if re.search(consecutive_digits_pattern, credit_card_number_modified):
+                print("Invalid")
+            else:
+                print("Valid")
         else:
-            print("Valid")
-    else:
-        print("Invalid")
+            print("Invalid")
